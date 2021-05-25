7ccf536d20d2c73f349c115bbf9a7ed3895079d9
Kerosz <andrei_chirila@outlook.com>
2021-05-25 20:14:12 +0300

    feature: added services spec && general config

---
+++ spec/services/images.spec.ts
@@ -0,0 +1,11 @@
+describe("Image Service", function () {
+  describe("resizeImage func", function () {
+    it("should return a string with the processed image path", function () {
+      expect(true).toBeTruthy();
+    });
+
+    it("should return a cached path if the image has been already processed", function () {
+      expect(true).toBeTruthy();
+    });
+  });
+});
--- spec/utils/file-system.spec.ts
+++ spec/utils/file-system.spec.ts
@@ -1,10 +1,32 @@
-describe("File system functions", () => {
-  describe("readFile func", () => {
-    it("should read a file and return a buffer", () => {
+import mock from "mock-fs";
+import path from "path";
+import { readFile } from "../../src/utils/file-system";
+
+describe("File System Utils", function () {
+  beforeAll(() => {
+    mock({
+      bucket: {
+        full: {
+          "spacex.jpg": Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+        },
+      },
+    });
+  });
+
+  afterAll(() => mock.restore());
+
+  describe("readFile func", function () {
+    it("should read a file and return a buffer", async function () {
+      const filePath = path.resolve(path.join("bucket", "full", "spacex.jpg"));
+
+      const result = await readFile(filePath);
+
+      console.log(result);
+
       expect(true).toBeTruthy();
     });
 
-    it("should return nothing when file does not exist", () => {
+    it("should return nothing when file does not exist", function () {
       expect(true).toBeTruthy();
     });
   });
